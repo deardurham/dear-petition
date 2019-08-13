dfcde8464db6e83f8a5f69042fb15224c3f5838b
Colin Copeland <copelco@caktusgroup.com>
2019-08-13 16:27:41 -0400

    default to .herokuapp.com on production settings

--- config/settings/production.py
+++ config/settings/production.py
@@ -15,7 +15,7 @@ from .base import env
 # https://docs.djangoproject.com/en/dev/ref/settings/#secret-key
 SECRET_KEY = env("DJANGO_SECRET_KEY")
 # https://docs.djangoproject.com/en/dev/ref/settings/#allowed-hosts
-ALLOWED_HOSTS = env.list("DJANGO_ALLOWED_HOSTS", default=["example.com"])
+ALLOWED_HOSTS = env.list("DJANGO_ALLOWED_HOSTS", default=[".herokuapp.com"])
 
 # DATABASES
 # ------------------------------------------------------------------------------
@@ -136,9 +136,7 @@ DEFAULT_FROM_EMAIL = env(
 # https://docs.djangoproject.com/en/dev/ref/settings/#server-email
 SERVER_EMAIL = env("DJANGO_SERVER_EMAIL", default=DEFAULT_FROM_EMAIL)
 # https://docs.djangoproject.com/en/dev/ref/settings/#email-subject-prefix
-EMAIL_SUBJECT_PREFIX = env(
-    "DJANGO_EMAIL_SUBJECT_PREFIX", default="[DEAR Petition]"
-)
+EMAIL_SUBJECT_PREFIX = env("DJANGO_EMAIL_SUBJECT_PREFIX", default="[DEAR Petition]")
 
 # ADMIN
 # ------------------------------------------------------------------------------
@@ -200,11 +198,7 @@ LOGGING = {
             "handlers": ["console"],
             "propagate": False,
         },
-        "ciprs": {
-            "level": APP_LOG_LEVEL,
-            "handlers": ["console"],
-            "propagate": False,
-        },
+        "ciprs": {"level": APP_LOG_LEVEL, "handlers": ["console"], "propagate": False},
         # Errors logged by the SDK itself
         "sentry_sdk": {"level": "ERROR", "handlers": ["console"], "propagate": False},
         "django.security.DisallowedHost": {
