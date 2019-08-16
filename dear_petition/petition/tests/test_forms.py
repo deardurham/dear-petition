a910d0fa09ca6182dcb179b2499f5abb1c39328e
Colin Copeland <copelco@caktusgroup.com>
2019-08-16 15:26:35 -0400

    add initial petition form tests

---
+++ dear_petition/petition/tests/test_forms.py
@@ -0,0 +1,31 @@
+import pytest
+from unittest.mock import MagicMock
+
+from dear_petition.petition.models import CIPRSRecord
+from dear_petition.petition.forms import GeneratePetitionForm
+
+
+class TestPetitionCheckboxes:
+    def test_district_checkbox_value_set(self):
+        record = CIPRSRecord(data={"General": {"District": "Yes"}})
+        form = GeneratePetitionForm(record=record)
+        form.clean()
+        assert form.record.data["General"]["District"] == "/Yes"
+
+    def test_district_checkbox_value_not_set(self):
+        record = CIPRSRecord(data={"General": {}})
+        form = GeneratePetitionForm(record=record)
+        form.clean()
+        assert "District" not in form.record.data["General"]
+
+    def test_superior_checkbox_value_set(self):
+        record = CIPRSRecord(data={"General": {"Superior": "Yes"}})
+        form = GeneratePetitionForm(record=record)
+        form.clean()
+        assert form.record.data["General"]["Superior"] == "/Yes"
+
+    def test_superior_checkbox_value_not_set(self):
+        record = CIPRSRecord(data={"General": {}})
+        form = GeneratePetitionForm(record=record)
+        form.clean()
+        assert "Superior" not in form.record.data["General"]
