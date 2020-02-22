6104ae2dcdc0ccf13406c37ef951510505d04a5b
Colin Copeland <copelco@caktusgroup.com>
2020-02-22 10:30:06 -0500

    fix migration script to include pre-patch records

--- dear_petition/petition/migrations/0008_auto_20200208_0222.py
+++ dear_petition/petition/migrations/0008_auto_20200208_0222.py
@@ -5,9 +5,15 @@ from django.db import migrations
 
 def move_batch_fks(apps, schema_editor):
     Batch = apps.get_model("petition", "Batch")
+    CIPRSRecord = apps.get_model("petition", "CIPRSRecord")
     for batch in Batch.objects.all():
         print(f"Adding batch {batch.pk} to {batch.records.count()} records")
         batch.records.update(batch=batch)
+    first_batch = Batch.objects.order_by("pk").first()
+    for record in CIPRSRecord.objects.all():
+        if not record.batch:
+            record.batch = first_batch
+            record.save()
 
 
 class Migration(migrations.Migration):
