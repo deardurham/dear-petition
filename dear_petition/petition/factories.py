c5794c640555d7bc8147cedea57ea276a1a33df8
George Helman <ghelman@archinsurance.com>
2020-03-01 16:21:26 -0500

    Added permission test and utilities

--- dear_petition/petition/factories.py
+++ dear_petition/petition/factories.py
@@ -1,8 +1,13 @@
-import factory
+import logging
 from datetime import datetime
+
+import factory
 
 import dear_petition.petition.models as models
 import dear_petition.users.models as user_model
+
+logger = logging.getLogger(__name__)
+
 
 class UserFactory(factory.django.DjangoModelFactory):
     class Meta:
--- dear_petition/petition/views.py
+++ dear_petition/petition/views.py
@@ -25,7 +25,7 @@ def upload_report(request):
 @login_required
 def view_record(request, pk):
     record = get_object_or_404(CIPRSRecord, pk=pk)
-    if not is_owner(request.user,record.batch) and not request.user.is_superuser:
+    if not is_owner(request.user,record.batch):
         raise PermissionDenied
     if "_meta" in record.data and "source" in record.data["_meta"]:
         source = record.data["_meta"]["source"]
