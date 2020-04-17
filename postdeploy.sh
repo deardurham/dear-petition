4241949b6348b0602ca140ce0b27b2b449303cf4
Colin Copeland <copelco@caktusgroup.com>
2020-04-17 16:33:15 -0400

    fix test user creation

--- postdeploy.sh
+++ postdeploy.sh
@@ -1,1 +1,1 @@
-echo "from django.contrib.auth.models import User; User.objects.create_superuser('qatester', 'qatester@example.com', 'qatester')" | python manage.py shell
+echo "from django.contrib.auth import get_user_model; get_user_model().objects.create_superuser('qatester', 'qatester@example.com', 'qatester')" | python manage.py shell
