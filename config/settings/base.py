d2672405413206b294b58f44e2f8ae50654ee14b
Christopher Dixon <cdixon@caktusgroup.com>
2020-04-20 12:56:45 -0400

    creates a user in db when review app is spun up.

--- config/settings/base.py
+++ config/settings/base.py
@@ -241,7 +241,7 @@ ACCOUNT_AUTHENTICATION_METHOD = "email"
 # https://django-allauth.readthedocs.io/en/latest/configuration.html
 ACCOUNT_EMAIL_REQUIRED = True
 # https://django-allauth.readthedocs.io/en/latest/configuration.html
-ACCOUNT_EMAIL_VERIFICATION = "mandatory"
+ACCOUNT_EMAIL_VERIFICATION = "optional"
 # https://django-allauth.readthedocs.io/en/latest/configuration.html
 ACCOUNT_ADAPTER = "dear_petition.users.adapters.AccountAdapter"
 # https://django-allauth.readthedocs.io/en/latest/configuration.html
