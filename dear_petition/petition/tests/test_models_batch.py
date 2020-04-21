6c772ef58fa245f27c0e0cb1ec0fcd79fe4259ff
Christopher Dixon <cdixon@caktusgroup.com>
2020-04-21 15:47:24 -0400

    updated test

--- dear_petition/petition/tests/test_models_batch.py
+++ dear_petition/petition/tests/test_models_batch.py
@@ -29,12 +29,22 @@ def test_get_petition_offenses():
     offense_record_4 = OffenseRecordFactory(offense=offense_2)
 
     petition_offenses = batch.get_petition_offenses()
-    assert f"Fileno:{4}" in petition_offenses
-    assert f"Fileno:{5}" not in petition_offenses
+    assert petition_offenses.get(f"Fileno:{4}").get("V") == ciprs_record_2.file_no
+    assert (
+        petition_offenses.get(f"Description:{4}").get("V")
+        == offense_record_4.description
+    )
+    assert petition_offenses.get(f"Fileno:{5}") is None
+    assert petition_offenses.get(f"Description:{5}") is None
 
     offense_record_4.action = ARRAIGNED
     offense_record_4.save()
     # Now only 3 offense records have the action of "CHARGED"
     petition_offenses = batch.get_petition_offenses()
-    assert f"Fileno:{3}" in petition_offenses
-    assert f"Fileno:{4}" not in petition_offenses
+    assert petition_offenses.get(f"Fileno:{3}").get("V") == ciprs_record_2.file_no
+    assert (
+        petition_offenses.get(f"Description:{3}").get("V")
+        == offense_record_4.description
+    )
+    assert petition_offenses.get(f"Fileno:{4}") is None
+    assert petition_offenses.get(f"Description:{4}") is None
