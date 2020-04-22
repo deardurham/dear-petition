da7265e93b13b3787358955baa1f31037f616947
Christopher Dixon <cdixon@caktusgroup.com>
2020-04-22 13:06:21 -0400

    updated test_get_petition_offenses in order account for not knowing the order of offenses being in petition_offenses

--- dear_petition/petition/tests/test_models_batch.py
+++ dear_petition/petition/tests/test_models_batch.py
@@ -18,33 +18,63 @@ def test_batch_most_recent_record_none(batch, record1):
 
 def test_get_petition_offenses():
     batch = BatchFactory()
-    ciprs_record_1 = CIPRSRecordFactory(batch=batch)
-    ciprs_record_2 = CIPRSRecordFactory(batch=batch)
+    ciprs_record_1 = CIPRSRecordFactory(batch=batch, file_no="CP-100")
+    ciprs_record_2 = CIPRSRecordFactory(batch=batch, file_no="CP-200")
     offense_1 = OffenseFactory(ciprs_record=ciprs_record_1)
     offense_2 = OffenseFactory(ciprs_record=ciprs_record_2)
     # OffenseRecordFactory defaults to "Action: Charged"
-    offense_record_1 = OffenseRecordFactory(offense=offense_1)
-    offense_record_2 = OffenseRecordFactory(offense=offense_1)
-    offense_record_3 = OffenseRecordFactory(offense=offense_2)
-    offense_record_4 = OffenseRecordFactory(offense=offense_2)
+    offense_record_1 = OffenseRecordFactory(
+        offense=offense_1, description="Offense Record 1"
+    )
+    offense_record_2 = OffenseRecordFactory(
+        offense=offense_1, description="Offense Record 2"
+    )
+    offense_record_3 = OffenseRecordFactory(
+        offense=offense_2, description="Offense Record 3"
+    )
+    offense_record_4 = OffenseRecordFactory(
+        offense=offense_2, description="Offense Record 4"
+    )
 
     petition_offenses = batch.get_petition_offenses()
-    assert petition_offenses.get(f"Fileno:{4}").get("V") == ciprs_record_2.file_no
-    assert (
-        petition_offenses.get(f"Description:{4}").get("V")
-        == offense_record_4.description
-    )
-    assert petition_offenses.get(f"Fileno:{5}") is None
-    assert petition_offenses.get(f"Description:{5}") is None
+    expected_file_no = [
+        offense_record_1.offense.ciprs_record.file_no,
+        offense_record_2.offense.ciprs_record.file_no,
+        offense_record_3.offense.ciprs_record.file_no,
+        offense_record_4.offense.ciprs_record.file_no,
+    ]
+    actual_file_no = []
+    for item in petition_offenses.items():
+        if "Fileno" in item[0]:
+            actual_file_no.extend(item[1])
+    assert len(expected_file_no) == len(actual_file_no)
 
+    # setting the action of the offense_record_4 arraigned
+    # should remove the offense_record from petition_offenses
+    # dictionary
+    # we'll check this by asserting that offense_record_4's
+    # description is not found in the dictionary
     offense_record_4.action = ARRAIGNED
     offense_record_4.save()
-    # Now only 3 offense records have the action of "CHARGED"
+
     petition_offenses = batch.get_petition_offenses()
-    assert petition_offenses.get(f"Fileno:{3}").get("V") == ciprs_record_2.file_no
-    assert (
-        petition_offenses.get(f"Description:{3}").get("V")
-        == offense_record_4.description
-    )
-    assert petition_offenses.get(f"Fileno:{4}") is None
-    assert petition_offenses.get(f"Description:{4}") is None
+    expected_file_no = [
+        offense_record_1.offense.ciprs_record.file_no,
+        offense_record_2.offense.ciprs_record.file_no,
+        offense_record_3.offense.ciprs_record.file_no,
+    ]
+    expected_descriptions = [
+        offense_record_1.description,
+        offense_record_2.description,
+        offense_record_3.description,
+    ]
+    actual_file_no = []
+    actual_descriptions = []
+    for item in petition_offenses.items():
+        if "Fileno" in item[0]:
+            actual_file_no.extend(item[1])
+        if "Description" in item[0]:
+            actual_descriptions.extend(item[1])
+    assert len(expected_file_no) == len(actual_file_no)
+    description_4 = offense_record_4.description
+    assert description_4 not in actual_descriptions
