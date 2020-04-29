b53c790b9b924fea235c79aa8ae80c8d50aa0e18
Christopher Dixon <cdixon@caktusgroup.com>
2020-04-29 14:18:03 -0400

    updated test.

--- dear_petition/petition/tests/test_models.py
+++ dear_petition/petition/tests/test_models.py
@@ -197,6 +197,27 @@ def test_refresh_record_from_data():
         "Arrest Date", dt_obj_to_date(ciprs_record.offense_date)
     )
 
+    # update file_no, sex, and offense_date attributes in the raw data dictionary and refresh the record
+    file_no = "ABC29304"
+    sex = "M"
+    offense_date = "2019-04-01T20:30:20"
+    ciprs_record.data["General"]["File No"] = file_no
+    ciprs_record.data["Defendant"]["Sex"] = sex
+    ciprs_record.data["Case Information"]["Offense Date"] = offense_date
+    ciprs_record.refresh_record_from_data()
+    # assert that the file_no, sex, and offense_date field has changed on the instance
+    assert ciprs_record.file_no == file_no
+    assert ciprs_record.sex == sex
+    assert ciprs_record.offense_date.strftime(
+        "%Y-%m-%dT%H:%M:%S"
+    ) == make_datetime_aware(offense_date).strftime("%Y-%m-%dT%H:%M:%S")
+
+
+def test_refreshing_offenses():
+    """Testing Offenses and OffenseRecord creation/deletion when refresh_record_from data is called"""
+    ciprs_record = CIPRSRecordFactory()
+    ciprs_record.refresh_record_from_data()
+
     # Offense Assertions
     assert ciprs_record.offenses.first().disposed_on.strftime(
         "%Y-%m-%d"
@@ -222,20 +243,75 @@ def test_refresh_record_from_data():
     for action in stored_actions:
         assert action in raw_data_actions
 
-    # update file_no, sex, and offense_date attributes in the raw data dictionary and refresh the record
-    file_no = "ABC29304"
-    sex = "M"
-    offense_date = "2019-04-01T20:30:20"
-    ciprs_record.data["General"]["File No"] = file_no
-    ciprs_record.data["Defendant"]["Sex"] = sex
-    ciprs_record.data["Case Information"]["Offense Date"] = offense_date
+    # Offense Record's Record key has changed on the raw data to only having 1 record.
+    # lets make sure there is only one record being stored and this is it.
+    new_o_record_code = "9999"
+    updated_o_records = {
+        "Records": [
+            {
+                "Action": "CONVICTED",
+                "Description": "IMPROPER EQUIP - SPEEDOMETER",
+                "Severity": "INFRACTION",
+                "Law": "G.S. 20-123.2",
+                "Code": new_o_record_code,
+            },
+        ]
+    }
+    ciprs_record.data.get("Offense Record").update(updated_o_records)
     ciprs_record.refresh_record_from_data()
-    # assert that the file_no, sex, and offense_date field has changed on the instance
-    assert ciprs_record.file_no == file_no
-    assert ciprs_record.sex == sex
-    assert ciprs_record.offense_date.strftime(
-        "%Y-%m-%dT%H:%M:%S"
-    ) == make_datetime_aware(offense_date).strftime("%Y-%m-%dT%H:%M:%S")
+
+    raw_data_offense_records = ciprs_record.data.get("Offense Record").get("Records")
+    stored_offense_records = list(ciprs_record.offenses.first().offense_records.all())
+    assert len(stored_offense_records) == len(raw_data_offense_records)
+    assert int(new_o_record_code) == stored_offense_records[0].code
+
+    # Now lets add three offenses and call refresh
+    o_record_code_list = ["1111", "2222", "3333", "4444"]
+    o_record_int_code_list = [int(code) for code in o_record_code_list]
+    updated_o_records = {
+        "Records": [
+            {
+                "Action": "CONVICTED",
+                "Description": "IMPROPER EQUIP - SPEEDOMETER",
+                "Severity": "INFRACTION",
+                "Law": "G.S. 20-123.2",
+                "Code": o_record_code_list[0],
+            },
+            {
+                "Action": "CHARGED",
+                "Description": "SPEEDING(80 mph in a 65 mph zone)",
+                "Severity": "INFRACTION",
+                "Law": "G.S. 20-141(B)",
+                "Code": o_record_code_list[1],
+            },
+            {
+                "Action": "ARRAIGNED",
+                "Description": "SPEEDING(80 mph in a 65 mph zone)",
+                "Severity": "INFRACTION",
+                "Law": "G.S. 20-141(B)",
+                "Code": o_record_code_list[2],
+            },
+            {
+                "Action": "CONVICTED",
+                "Description": "IMPROPER EQUIP - SPEEDOMETER",
+                "Severity": "INFRACTION",
+                "Law": "G.S. 20-123.2",
+                "Code": o_record_code_list[3],
+            },
+        ]
+    }
+    ciprs_record.data.get("Offense Record").update(updated_o_records)
+    ciprs_record.refresh_record_from_data()
+
+    raw_data_offense_records = ciprs_record.data.get("Offense Record").get("Records")
+    stored_codes = []
+    stored_offense_records = list(ciprs_record.offenses.first().offense_records.all())
+    for offense_record in stored_offense_records:
+        stored_codes.append(offense_record.code)
+    assert len(stored_offense_records) == len(raw_data_offense_records)
+    assert len(stored_actions) == len(raw_data_actions)
+    for code in stored_codes:
+        assert code in o_record_int_code_list
 
 
 def test_refresh_record_from_data_empty_data_dict():
