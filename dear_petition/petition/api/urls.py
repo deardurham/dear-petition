524636d028c3612118ed030c8cab62b79ecb1376
Christopher Dixon <cdixon@caktusgroup.com>
2020-05-02 00:50:49 -0400

    Users using api are now required to request a jwt access token

--- dear_petition/petition/api/urls.py
+++ dear_petition/petition/api/urls.py
@@ -1,5 +1,9 @@
 from django.urls import include, path
 from rest_framework import routers
+from rest_framework_simplejwt.views import (
+    TokenObtainPairView,
+    TokenRefreshView,
+)
 from . import viewsets
 
 router = routers.DefaultRouter()
@@ -12,4 +16,6 @@ app_name = "api"
 
 urlpatterns = [
     path("", include(router.urls)),
+    path("token/", TokenObtainPairView.as_view(), name="token_obtain_pair"),
+    path("token/refresh/", TokenRefreshView.as_view(), name="token_refresh"),
 ]
