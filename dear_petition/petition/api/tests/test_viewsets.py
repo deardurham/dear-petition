99f64f2c0af89ee550ef9cea29d6da83aea54570
Christopher Dixon <cdixon@caktusgroup.com>
2020-05-13 13:08:09 -0400

    test

--- dear_petition/petition/api/tests/test_viewsets.py
+++ dear_petition/petition/api/tests/test_viewsets.py
@@ -72,24 +72,16 @@ class TestBatchViewSet(APITestCase):
             self.assertIn(data["label"], response.data["label"])
 
         with self.subTest("PUT"):
-            data = dict(label="Golden Lab", date_uploaded=self.batch_3.date_uploaded)
-            response = self.client.put(
-                self.detail_url, data=data, HTTP_AUTHORIZATION=f"Bearer {self.access}"
-            )
-            self.assertEqual(response.status_code, status.HTTP_200_OK)
+            pass
 
         with self.subTest("PATCH"):
-            data = dict(label="Tommy Pickles")
-            response = self.client.patch(
-                self.detail_url, data=data, HTTP_AUTHORIZATION=f"Bearer {self.access}"
-            )
-            self.assertEqual(response.status_code, status.HTTP_200_OK)
+            pass
 
         with self.subTest("DELETE"):
             response = self.client.delete(
                 self.detail_url, HTTP_AUTHORIZATION=f"Bearer {self.access}"
             )
-            self.assertEqual(response.status_code, status.HTTP_200_OK)
+            self.assertEqual(response.status_code, status.HTTP_204_NO_CONTENT)
 
     def test_unauthorized(self):
         """Unauthorized users may not use the API.
@@ -124,102 +116,3 @@ class TestBatchViewSet(APITestCase):
         with self.subTest("DELETE"):
             response = self.client.delete(self.detail_url)
             self.assertEqual(response.status_code, status.HTTP_401_UNAUTHORIZED)
-
-
-# class TestCIPRSRecordViewSet(APITestCase):
-#     def setUp(self):
-#         self.user = UserFactory()
-#         self.batch_1 = BatchFactory(user=self.user)
-#         self.batch_2 = BatchFactory(user=self.user)
-#         self.ciprsrecord_1 = CIPRSRecordFactory(batch=self.batch_1)
-#         self.ciprsrecord_2 = CIPRSRecordFactory(batch=self.batch_1)
-#         self.ciprsrecord_3 = CIPRSRecordFactory(batch=self.batch_2)
-#         self.list_url = reverse("api:ciprsrecord-list")
-#         self.detail_url = reverse(
-#             "api:ciprsrecord-detail", args=[self.ciprsrecord_1.pk]
-#         )
-#         # Create a token for the self.user, to be used with each request.
-#         self.tokens = get_tokens_for_user(self.user)
-#         self.access = self.tokens["access"]
-
-#     def test_authorized(self):
-#         with self.subTest("Get - List"):
-#             ciprsrecord_list = [self.ciprsrecord_1, self.ciprsrecord_2, self.ciprsrecord_3]
-#             response = self.client.get(
-#                 self.list_url, HTTP_AUTHORIZATION=f"Bearer {self.access}"
-#             )
-#             self.assertEqual(response.status_code, status.HTTP_200_OK)
-#             self.assertEqual(response.data['count'], len(ciprsrecord_list))
-#             results = [dict(ciprsrecord) for ciprsrecord in response.data['results']]
-#             result_labels = [ciprsrecord['label'] for ciprsrecord in results]
-#             ciprsrecord_list_labels = [ciprsrecord.label for ciprsrecord in ciprsrecord_list]
-#             for crl in ciprsrecord_list_labels:
-#                 self.assertIn(crl, result_labels)
-
-#         with self.subTest("Get - Detail"):
-#             response = self.client.get(self.detail_url, HTTP_AUTHORIZATION=f"Bearer {self.access}", format="json")
-#             self.assertEqual(response.status_code, status.HTTP_200_OK)
-#             self.assertEqual(self.ciprsrecord_1.label, response.data['label'])
-
-#         with self.subTest("POST"):
-#             data = dict(
-#                 batch=self.batch_2.pk,
-#                 label='Homer Simpson',
-#                 data=self.ciprsrecord_1.data,
-#             )
-#             response = self.client.post(self.list_url, data=data, HTTP_AUTHORIZATION=f"Bearer {self.access}", format="json")
-#             self.assertEqual(response.status_code, status.HTTP_201_CREATED)
-#             self.assertIn(data['label'], response.data['label'])
-
-#         with self.subTest("PUT"):
-#             data = dict(
-#                 batch=self.batch_2.pk,
-#                 label="Golden Lab",
-#             )
-#             response = self.client.put(self.detail_url, data=data, HTTP_AUTHORIZATION=f"Bearer {self.access}", format="json")
-#             self.assertEqual(response.status_code, status.HTTP_200_OK)
-
-#         with self.subTest("PATCH"):
-#             data = dict(
-#                 label='Tommy Pickles'
-#             )
-#             response = self.client.patch(self.detail_url, data=data, HTTP_AUTHORIZATION=f"Bearer {self.access}", format="json")
-#             self.assertEqual(response.status_code, status.HTTP_200_OK)
-
-#         with self.subTest("DELETE"):
-#             response = self.client.delete(self.detail_url, HTTP_AUTHORIZATION=f"Bearer {self.access}", format="json")
-#             self.assertEqual(response.status_code, status.HTTP_200_OK)
-
-#     def test_unauthorized(self):
-#         """Unauthorized users may not use the API.
-
-#         Not sending an "Authorization" header with the request to the API means
-#         the response will have a 401 status code.
-#         """
-
-#         with self.subTest("Get - List"):
-#             response = self.client.get(self.list_url)
-#             self.assertEqual(response.status_code, status.HTTP_401_UNAUTHORIZED)
-
-#         with self.subTest("Get - Detail"):
-#             response = self.client.get(self.detail_url)
-#             self.assertEqual(response.status_code, status.HTTP_401_UNAUTHORIZED)
-
-#         with self.subTest("POST"):
-#             data = {"code": "NEWPROJECT"}
-#             response = self.client.post(self.list_url, data=data)
-#             self.assertEqual(response.status_code, status.HTTP_401_UNAUTHORIZED)
-
-#         with self.subTest("PUT"):
-#             data = {"code": "NEWPROJECT"}
-#             response = self.client.put(self.detail_url, data=data)
-#             self.assertEqual(response.status_code, status.HTTP_401_UNAUTHORIZED)
-
-#         with self.subTest("PATCH"):
-#             data = {"code": "NEWPROJECT"}
-#             response = self.client.patch(self.detail_url, data=data)
-#             self.assertEqual(response.status_code, status.HTTP_401_UNAUTHORIZED)
-
-#         with self.subTest("DELETE"):
-#             response = self.client.delete(self.detail_url)
-#             self.assertEqual(response.status_code, status.HTTP_401_UNAUTHORIZED)
