38e4c2d98730da264083f1a6f8d4395bf5b579a4
Colin Copeland <copelco@caktusgroup.com>
2020-05-17 13:56:16 -0400

    stub basic etl tests

--- dear_petition/petition/etl/load.py
+++ dear_petition/petition/etl/load.py
@@ -1,3 +1,5 @@
+import logging
+
 from django.conf import settings
 from django import forms
 
@@ -7,11 +9,16 @@ from dear_petition.petition.etl.extract import parse_ciprs_document
 
 __all__ = ("import_ciprs_records",)
 
+logger = logging.getLogger(__name__)
+
 
 def import_ciprs_records(files, user):
     """Import uploaded CIPRS records into models."""
+    logger.info("Importing CIPRS records")
     batch = Batch.objects.create(user=user)
+    logger.info(f"Created batch {batch.id}")
     for idx, file_ in enumerate(files):
+        logger.info(f"Importing file {file_}")
         record = CIPRSRecord(batch=batch)
         if settings.CIPRS_SAVE_PDF:
             record.report_pdf = file_
