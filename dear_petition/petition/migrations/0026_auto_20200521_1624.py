435581ab4d8306bccc200db30f07b523ae646c2f
Christopher Dixon <cdixon@caktusgroup.com>
2020-05-22 13:56:11 -0400

    corrected typo

--- dear_petition/petition/migrations/0026_auto_20200521_1624.py
+++ dear_petition/petition/migrations/0026_auto_20200521_1624.py
@@ -7,7 +7,7 @@ import django.db.models.deletion
 class Migration(migrations.Migration):
 
     dependencies = [
-        ("petition", "025_auto_20200520_0120"),
+        ("petition", "0025_auto_20200520_0120"),
     ]
 
     operations = [
