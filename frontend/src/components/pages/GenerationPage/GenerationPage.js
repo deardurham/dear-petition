3ae52cfd1c0727832c211636f8063643ff51d14c
Colin Copeland <copelco@caktusgroup.com>
2020-05-24 15:43:26 -0400

    add GenerationContext

--- frontend/src/components/pages/GenerationPage/GenerationPage.js
+++ frontend/src/components/pages/GenerationPage/GenerationPage.js
@@ -1,4 +1,4 @@
-import React, { useState, useEffect } from 'react';
+import React, { useState, useEffect, createContext } from 'react';
 import {
   GenerationPageStyled,
   PetitionsList,
@@ -17,35 +17,56 @@ const FAKE_RESPONSE = {
   ]
 };
 
+export const GenerationContext = createContext(null);
+
 function GenerationPage(props) {
   const { batchId } = useParams();
   const [loading, setLoading] = useState();
   const [batch, setBatch] = useState();
+  const [petition, setPetition] = useState();
+  const [ssn, setSSN] = useState('');
+  const [license, setLicense] = useState('');
+  const [attorney, setAttorney] = useState('');
 
   useEffect(() => {
     setLoading(true);
-    let timeout = setTimeout(() => {
+    setTimeout(() => {
       setBatch(FAKE_RESPONSE);
       setLoading(false);
     }, 1000);
   }, [batchId]);
 
+  const context = {
+    batch,
+    setBatch,
+    petition,
+    setPetition,
+    ssn,
+    setSSN,
+    license,
+    setLicense,
+    attorney,
+    setAttorney,
+  }
+
   return (
-    <GenerationPageStyled>
-      {loading ? (
-        <h2>Loading...</h2>
-      ) : (
-        <GenerationContentStyled>
-          <h2>{batch?.label}</h2>
-          <GenerationInputs s />
-          <PetitionsList>
-            {batch?.petitions?.map(petition => {
-              return <PetitionListItem key={petition.id} petition={petition} />;
-            })}
-          </PetitionsList>
-        </GenerationContentStyled>
-      )}
-    </GenerationPageStyled>
+    <GenerationContext.Provider value={context}>
+      <GenerationPageStyled>
+        {loading ? (
+          <h2>Loading...</h2>
+        ) : (
+            <GenerationContentStyled>
+              <h2>{batch?.label}</h2>
+              <GenerationInputs s />
+              <PetitionsList>
+                {batch?.petitions?.map(petition => {
+                  return <PetitionListItem key={petition.id} petition={petition} />;
+                })}
+              </PetitionsList>
+            </GenerationContentStyled>
+          )}
+      </GenerationPageStyled>
+    </GenerationContext.Provider >
   );
 }
 
--- frontend/src/components/pages/GenerationPage/PetitionListItem.js
+++ frontend/src/components/pages/GenerationPage/PetitionListItem.js
@@ -1,16 +1,30 @@
-import React from 'react';
+import React, { useState, useContext } from 'react';
 import { PetitionListItemStyled, PetitionCellStyled } from './PetitionListItem.styled';
+import { ModalStyled, ModalContent } from '../HomePage/HomePage.styled'
+import { GenerationContext } from './GenerationPage';
 
 function PetitionListItem({ petition }) {
+    const [showModal, setShowModal] = useState(false);
+    const { batch, setPetition, ssn } = useContext(GenerationContext);
+
     const handlePetitionSelect = () => {
-        // do things
+        setPetition(petition);
+        setShowModal(true);
+        console.log(ssn)
     }
     return (
-        <PetitionListItemStyled onClick={handlePetitionSelect}>
-            <PetitionCellStyled>{petition.type}</PetitionCellStyled>
-            <PetitionCellStyled>{petition.county} County</PetitionCellStyled>
-            <PetitionCellStyled>{petition.court} court</PetitionCellStyled>
-        </PetitionListItemStyled>
+        <>
+            <PetitionListItemStyled onClick={handlePetitionSelect}>
+                <PetitionCellStyled>{petition.type}</PetitionCellStyled>
+                <PetitionCellStyled>{petition.county} County</PetitionCellStyled>
+                <PetitionCellStyled>{petition.court} court</PetitionCellStyled>
+            </PetitionListItemStyled>
+            <ModalStyled isVisible={showModal} closeModal={() => setShowModal(false)}>
+                <ModalContent>
+                    <h2>{petition.type}</h2>
+                </ModalContent>
+            </ModalStyled>
+        </>
     );
 }
 
