50524c510bd5dad0a1058bf6b84f53339a2e1fd7
Michael Ashton <michael.ashton@caktusgroup.com>
2020-05-27 10:41:45 -0400

    add close to generation modal

--- frontend/src/components/pages/GenerationPage/GeneratePetitionModal/GeneratePetitionModal.js
+++ frontend/src/components/pages/GenerationPage/GeneratePetitionModal/GeneratePetitionModal.js
@@ -1,37 +1,39 @@
 import React, { useContext } from 'react';
 import styled from 'styled-components';
-import { ModalStyled, ModalContent } from '../../HomePage/HomePage.styled'
+import { ModalStyled, ModalContent } from '../../HomePage/HomePage.styled';
 import { GenerationContext } from '../GenerationPage';
 import useKeyPress from '../../../../hooks/useKeyPress';
 import AgencyAutocomplete from './AgencyAutocomplete';
 import Button from '../../../elements/Button/Button';
 
 const GeneratePetitionModal = ({ closeModal, isVisible }) => {
-    const { petition, selectedAgencies } = useContext(GenerationContext);
+  const { petition, selectedAgencies } = useContext(GenerationContext);
 
-    useKeyPress('Escape', closeModal);
+  useKeyPress('Escape', closeModal);
 
-    const handleGenerate = () => {
-        console.log(selectedAgencies);
-    }
+  const handleGenerate = () => {
+    console.log(selectedAgencies);
+  };
 
-    return (
-        <GeneratePetitionModalStyled isVisible={isVisible}>
-            <ModalContent>
-                {petition ? (
-                    <>
-                        <h2>{petition.type}</h2>
-                        <ul>
-                            <li>Jurisdiction: {petition.court}</li>
-                            <li>County: {petition.county} County</li>
-                        </ul>
-                        <AgencyAutocomplete />
-                        <Button onClick={handleGenerate}>Generate</Button>
-                    </>
-                ) : ''}
-            </ModalContent>
-        </GeneratePetitionModalStyled>
-    );
+  return (
+    <GeneratePetitionModalStyled isVisible={isVisible} closeModal={closeModal}>
+      <ModalContent>
+        {petition ? (
+          <>
+            <h2>{petition.type}</h2>
+            <ul>
+              <li>Jurisdiction: {petition.court}</li>
+              <li>County: {petition.county} County</li>
+            </ul>
+            <AgencyAutocomplete />
+            <Button onClick={handleGenerate}>Generate</Button>
+          </>
+        ) : (
+          ''
+        )}
+      </ModalContent>
+    </GeneratePetitionModalStyled>
+  );
 };
 
 const GeneratePetitionModalStyled = styled(ModalStyled)``;
