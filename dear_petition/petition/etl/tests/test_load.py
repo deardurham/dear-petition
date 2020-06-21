df8dfa983697b1c78f2951766eb5478b6f9867ac
Colin Copeland <copelco@caktusgroup.com>
2020-06-20 21:43:42 -0400

    update test to expect list

--- dear_petition/petition/etl/tests/test_load.py
+++ dear_petition/petition/etl/tests/test_load.py
@@ -10,7 +10,7 @@ pytestmark = pytest.mark.django_db
 def test_import_ciprs_records(fake_pdf, user, mock_ciprs_reader):
     """Test basic import_ciprs_records() without testing full ETL."""
     record = {"Defendant": {"Name": "Jon Doe"}}
-    mock_ciprs_reader.return_value = record
+    mock_ciprs_reader.return_value = [record]
     batch = import_ciprs_records([fake_pdf], user)
     assert Batch.objects.count() == 1
     assert batch.label == record["Defendant"]["Name"]
@@ -20,7 +20,7 @@ def test_import_ciprs_records(fake_pdf, user, mock_ciprs_reader):
 def test_import_ciprs_records_multi_files(fake_pdf, fake_pdf2, user, mock_ciprs_reader):
     """Test basic import_ciprs_records() with multiple files without testing full ETL."""
     record = {"Defendant": {"Name": "Jon Doe"}}
-    mock_ciprs_reader.return_value = record
+    mock_ciprs_reader.return_value = [record]
     batch = import_ciprs_records([fake_pdf, fake_pdf2], user)
     assert Batch.objects.count() == 1
     assert batch.label == record["Defendant"]["Name"]
