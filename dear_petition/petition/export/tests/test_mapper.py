bfef186c6c9dd9c325b6ac6b17b20a8be73fa31b
Colin Copeland <copelco@caktusgroup.com>
2020-06-23 18:31:13 -0400

    remove breakpoint

--- dear_petition/petition/export/tests/test_mapper.py
+++ dear_petition/petition/export/tests/test_mapper.py
@@ -249,7 +249,6 @@ def test_map_offenses__offense_date(data, petition, record2, offense1, offense_r
 def test_map_offenses__disposition_method(
     data, petition, record2, offense1, offense_record1
 ):
-    breakpoint()
     mapper.map_offenses(data, petition)
     assert (
         data["Disposition:1"]
