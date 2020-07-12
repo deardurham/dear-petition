d4cba19f3d125f831d901b682adc28a7d5c0c4f1
Colin Copeland <copelco@caktusgroup.com>
2020-07-11 21:42:20 -0400

    use dt_obj_to_date

--- dear_petition/petition/utils.py
+++ dear_petition/petition/utils.py
@@ -48,7 +48,7 @@ def make_datetime_aware(dt_str):
     return aware_datetime_obj
 
 
-def format_petition_date(date):
+def format_petition_date(d):
     """ Format Date Objects for PDF Writer
 
     If date is true then the date object will be formatted
@@ -56,4 +56,6 @@ def format_petition_date(date):
     empty strings. Both of which can be written to the PDF
     correctly.
     """
-    return date.strftime(DATE_FORMAT) if date else ""
+    if isinstance(d, datetime):
+        d = dt_obj_to_date(d)
+    return d.strftime(DATE_FORMAT) if d else ""
