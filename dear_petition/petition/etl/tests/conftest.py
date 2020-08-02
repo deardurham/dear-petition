7c5a15241070f13fcd504afb0312af6f78d06e6a
Colin Copeland <copelco@caktusgroup.com>
2020-08-02 11:08:02 -0400

    add mock_transform_ciprs_document

--- dear_petition/petition/etl/tests/conftest.py
+++ dear_petition/petition/etl/tests/conftest.py
@@ -7,3 +7,11 @@ import pytest
 def mock_ciprs_reader():
     with mock.patch("dear_petition.petition.etl.load.parse_ciprs_document") as mock_:
         yield mock_
+
+
+@pytest.fixture
+def mock_transform_ciprs_document():
+    with mock.patch(
+        "dear_petition.petition.etl.extract.transform_ciprs_document"
+    ) as mock_:
+        yield mock_
