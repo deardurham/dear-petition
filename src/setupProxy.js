d011e3069b2a9b9a80df6c788ce79c780f9d6417
Rob Gries <robert.w.gries@gmail.com>
2021-02-15 21:44:44 -0500

    Add password reset links as proxied links

--- src/setupProxy.js
+++ src/setupProxy.js
@@ -2,14 +2,16 @@ const proxyMiddleware = require('http-proxy-middleware');
 
 // docker-compose will avoid using the fallback due to always having one of OVERRIDE_API_PROXY or API_PROXY set
 // Note: non-docker frontend needs to set one of the env vars to `http://localhost:8000` to proxy local backend
-const FALLBACK_PROXY = "https://dear-petition-staging.herokuapp.com/";
+const FALLBACK_PROXY = 'https://dear-petition-staging.herokuapp.com/';
 
-module.exports = function(app) {
-    const proxyOptions = proxyMiddleware({
-        target: process.env.OVERRIDE_API_PROXY || process.env.API_PROXY || FALLBACK_PROXY,
-        changeOrigin: true,
-    });
-    app.use('/petition/api', proxyOptions);
-    app.use('/admin(-\\w+)?/', proxyOptions);
-    app.use('/static/admin', proxyOptions);
+module.exports = app => {
+  const proxyOptions = proxyMiddleware({
+    target: process.env.OVERRIDE_API_PROXY || process.env.API_PROXY || FALLBACK_PROXY,
+    changeOrigin: true
+  });
+  app.use('/petition/api', proxyOptions);
+  app.use('/admin(-\\w+)?/', proxyOptions);
+  app.use('/static/admin', proxyOptions);
+  app.use('/password_reset/', proxyOptions);
+  app.use('/reset/', proxyOptions);
 };
