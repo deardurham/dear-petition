71a5032bb175d094ea1a3e6a55f41a6757d154eb
Rob Gries <robert.w.gries@gmail.com>
2021-06-19 15:17:27 -0400

    Remove unnecessary packages and fix random eslint issues

--- src/components/App.js
+++ src/components/App.js
@@ -1,7 +1,6 @@
 import React from 'react';
 import { AppStyled } from './App.styled';
 import GlobalStyle from '../styles/GlobalStyle';
-import { Provider as AlertProvider } from 'react-alert';
 
 // Routing
 import { BrowserRouter, Switch } from 'react-router-dom';
@@ -11,34 +10,25 @@ import ProtectedRoute from './containers/ProtectedRoute';
 import HomePage from './pages/HomePage/HomePage';
 import GenerationPage from './pages/GenerationPage/GenerationPage';
 import FAQPage from './pages/HelpPage/HelpPage';
-import Alert from './elements/Alert/Alert';
-
-const alertOptions = {
-  position: 'bottom right',
-  timeout: 5000,
-  transition: 'scale'
-};
 
 function App() {
   return (
     <>
       <GlobalStyle />
       <BrowserRouter>
-        <AlertProvider template={Alert} {...alertOptions}>
-          <AppStyled>
-            <Switch>
-              <ProtectedRoute exact path="/">
-                <HomePage />
-              </ProtectedRoute>
-              <ProtectedRoute exact path="/generate/:batchId">
-                <GenerationPage />
-              </ProtectedRoute>
-              <ProtectedRoute exact path="/help">
-                <FAQPage />
-              </ProtectedRoute>
-            </Switch>
-          </AppStyled>
-        </AlertProvider>
+        <AppStyled>
+          <Switch>
+            <ProtectedRoute exact path="/">
+              <HomePage />
+            </ProtectedRoute>
+            <ProtectedRoute exact path="/generate/:batchId">
+              <GenerationPage />
+            </ProtectedRoute>
+            <ProtectedRoute exact path="/help">
+              <FAQPage />
+            </ProtectedRoute>
+          </Switch>
+        </AppStyled>
       </BrowserRouter>
     </>
   );
