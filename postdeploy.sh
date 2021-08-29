8660144e4e1e7753b4630f7616bd9b18c91480b8
Colin Copeland <copelco@caktusgroup.com>
2021-08-29 15:54:17 -0400

    add heroku.yml

--- postdeploy.sh
+++ postdeploy.sh
@@ -1,2 +1,7 @@
 #!/bin/sh
+set -e
+
+>&2 echo "Running Migrations"
+python manage.py migrate --noinput
+
 echo "from django.contrib.auth import get_user_model; get_user_model().objects.create_superuser('qatester', 'qatester@example.com', 'qatester')" | python manage.py shell
