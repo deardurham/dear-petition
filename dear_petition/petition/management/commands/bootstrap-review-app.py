ed08838cd79a3b1bd1fc31f2e1e5b3880eb737a1
Colin Copeland <copelco@caktusgroup.com>
2021-08-29 21:32:43 -0400

    add bootstrap-review-app cmd

---
+++ dear_petition/petition/management/commands/bootstrap-review-app.py
@@ -0,0 +1,17 @@
+import os
+
+from django.contrib.auth import get_user_model  # pragma: no cover
+from django.core.management.base import BaseCommand  # pragma: no cover
+
+
+class Command(BaseCommand):  # pragma: no cover
+    help = "Bootstrap Heroku review app"
+
+    def handle(self, *args, **options):
+        User = get_user_model()
+        qatester_exists = User.objects.filter(email="qatester@example.com").exists()
+        if os.getenv("IS_REVIEW", "False") != "True" or qatester_exists:
+            self.stdout.write("**Not running bootstrap tasks**")
+            return
+        User.objects.create_superuser(email="qatester@example.com", password="qatester")
+        self.stdout.write(self.style.SUCCESS("Successfully created qatester"))
--- postdeploy.sh
+++ postdeploy.sh
@@ -4,4 +4,5 @@ set -e
 >&2 echo "Running Migrations"
 python manage.py migrate --noinput
 
-echo "from django.contrib.auth import get_user_model; get_user_model().objects.create_superuser('qatester', 'qatester@example.com', 'qatester')" | python manage.py shell
+>&2 echo "Running bootstrap-review-app"
+python manage.py bootstrap-review-app
