58920df77f6b1489c9cea60b5531f49eb6fb8099
Rob Gries <robert.w.gries@gmail.com>
2021-12-08 21:54:00 -0500

    Fix time delta

--- dear_petition/petition/api/viewsets.py
+++ dear_petition/petition/api/viewsets.py
@@ -188,7 +188,7 @@ class TokenObtainPairCookieView(simplejwt_views.TokenObtainPairView):
             serializer.validated_data[
                 "access"
             ],  # pull access token out of validated_data
-            max_age=settings.SIMPLE_JWT["ACCESS_TOKEN_LIFETIME"],
+            max_age=settings.SIMPLE_JWT["ACCESS_TOKEN_LIFETIME"].total_seconds(),
             domain=getattr(
                 settings, "AUTH_COOKIE_DOMAIN", None
             ),  # we can tie the cookie to a specific domain for added security
@@ -202,7 +202,7 @@ class TokenObtainPairCookieView(simplejwt_views.TokenObtainPairView):
         response.set_cookie(
             settings.REFRESH_COOKIE_KEY,
             serializer.validated_data["refresh"],
-            max_age=settings.SIMPLE_JWT["REFRESH_TOKEN_LIFETIME"],
+            max_age=settings.SIMPLE_JWT["REFRESH_TOKEN_LIFETIME"].total_seconds(),
             domain=getattr(settings, "AUTH_COOKIE_DOMAIN", None),
             path=self.cookie_path,
             secure=settings.DEBUG == False,  # browsers should only send the cookie using HTTPS
