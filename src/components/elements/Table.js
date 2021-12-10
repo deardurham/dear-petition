b502a94972df08ace678c43c50b7db54497ea3fa
Rob Gries <robert.w.gries@gmail.com>
2021-12-09 20:26:19 -0500

    Fix issue

--- src/components/elements/Table.js
+++ src/components/elements/Table.js
@@ -1,6 +1,6 @@
 import React from 'react';
 import styled from 'styled-components';
-import { colorPrimary } from '../../styles/colors';
+import { colorPrimary, greyScale } from '../../styles/colors';
 
 export const TableStyle = styled.table`
   display: grid;
