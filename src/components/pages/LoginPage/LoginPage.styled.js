bc60b48dcd205657cc851e46bbc870d741bfdc21
Rob Gries <robert.w.gries@gmail.com>
2022-02-19 01:03:05 -0500

    Cleanup login css

--- src/components/pages/LoginPage/LoginPage.styled.js
+++ src/components/pages/LoginPage/LoginPage.styled.js
@@ -3,31 +3,9 @@ import FormInput from '../../elements/Input/FormInput';
 import { colorRed } from '../../../styles/colors';
 import { motion } from 'framer-motion';
 
-export const LoginPageStyled = styled.main`
-  display: flex;
-  flex-direction: column;
-  align-items: center;
-  width: 100vw;
-`;
-
-export const LoginSplash = styled.div`
-  margin-top: calc(10rem + 2vw);
-  max-width: 1000px;
-  padding: 1rem;
-`;
-
 export const SplashLogo = styled.img`
   width: 100%;
   height: auto;
-`;
-
-export const LoginForm = styled.form`
-  margin-top: 10vh;
-  flex: 1;
-  width: 100%;
-  display: flex;
-  flex-direction: column;
-  align-items: center;
 `;
 
 export const FormErrors = styled(motion.div)`
@@ -46,12 +24,4 @@ export const InputStyled = styled(FormInput)`
 
 export const PasswordInputStyled = styled(InputStyled)`
   margin: 0;
-`;
-
-export const ForgotPassword = styled.a`
-  margin: 0;
-`;
-
-export const PasswordWrapper = styled.div`
-  margin-bottom 2rem;
 `;
