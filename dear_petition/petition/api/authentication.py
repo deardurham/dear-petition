29ec1f07cdb3bea3e625c2a978cd180f2496cb79
Rob Gries <robert.w.gries@gmail.com>
2022-06-18 15:45:34 -0400

    Re-authenticate if post/put request is missing auth header

--- dear_petition/petition/api/authentication.py
+++ dear_petition/petition/api/authentication.py
@@ -1,6 +1,6 @@
 from rest_framework_simplejwt.authentication import JWTAuthentication
-from rest_framework.authentication import CSRFCheck
-from rest_framework.exceptions import PermissionDenied
+from rest_framework.authentication import CSRFCheck, SessionAuthentication as DRFSessionAuthentication
+from rest_framework.exceptions import NotAuthenticated
 from django.conf import settings
 
 
@@ -13,7 +13,7 @@ def enforce_csrf(request):
     reason = check.process_view(request, None, (), {})
     if reason:
         # CSRF failed, bail with explicit error message
-        raise PermissionDenied("CSRF validation failed: %s" % reason)
+        raise NotAuthenticated("CSRF validation failed: %s" % reason)
 
 
 class JWTHttpOnlyCookieAuthentication(JWTAuthentication):
@@ -42,3 +42,18 @@ class JWTHttpOnlyCookieAuthentication(JWTAuthentication):
             return None
 
         return validated_user, validated_token
+
+
+# Lifted from csdenboer's suggestion on Github: https://github.com/encode/django-rest-framework/issues/5968#issuecomment-399352828
+class SessionAuthentication(DRFSessionAuthentication):
+    """
+    This class is needed, because REST Framework's default SessionAuthentication does never return 401's,
+    because they cannot fill the WWW-Authenticate header with a valid value in the 401 response. As a
+    result, we cannot distinguish calls that are not unauthorized (401 unauthorized) and calls for which
+    the user does not have permission (403 forbidden). See https://github.com/encode/django-rest-framework/issues/5968
+
+    We do set authenticate_header function in SessionAuthentication, so that a value for the WWW-Authenticate
+    header can be retrieved and the response code is automatically set to 401 in case of unauthenticated requests.
+    """
+    def authenticate_header(self, request):
+        return 'Session'
