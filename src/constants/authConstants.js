18afc3f4c2513cc8c973d61e7ef99dec6ef71ea0
Colin Copeland <copelco@caktusgroup.com>
2022-07-04 16:32:43 -0400

    use dash in csrf header name

--- src/constants/authConstants.js
+++ src/constants/authConstants.js
@@ -1,4 +1,4 @@
-export const CSRF_HEADER_KEY = 'X_CSRFTOKEN';
+export const CSRF_HEADER_KEY = 'X-CSRFTOKEN';
 export const CSRF_COOKIE_NAME = 'csrftoken';
 export const CSRF_TOKEN_LS_KEY = 'CSRF_TOKEN';
 export const JWT_COOKIE_KEY = 'Authorization';
