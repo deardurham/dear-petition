34c034c39f0e4a0646ce8f55e88d1e3275979074
Rebecca Drabenstott <rdrabenstott@gmail.com>
2022-12-20 19:12:24 -0500

    Flesh out str and repr implementations for models (#349)
    Co-authored-by: Rebecca Drabenstott <rdraenstott@gmail.com>

--- dear_petition/users/models.py
+++ dear_petition/users/models.py
@@ -1,16 +1,15 @@
 from django.contrib.auth.models import AbstractUser
 from django.db.models import CharField, DateField, ForeignKey
-from django.urls import reverse
 from django.utils.translation import ugettext_lazy as _
 from django.conf import settings
 from django.urls import reverse
 from django.core.mail import send_mail
 
-
 from . import constants as uc
+from ..common.models import PrintableModelMixin
 
 
-class User(AbstractUser):
+class User(PrintableModelMixin, AbstractUser):
 
     name = CharField(_("Name of User"), blank=True, max_length=uc.NAME_MAX_LENGTH)
     last_generated_petition_time = DateField(null=True)
@@ -21,3 +20,13 @@ class User(AbstractUser):
     def send_email(self, subject, message, send_anyway=False):
         if settings.ENVIRONMENT == "PRODUCTION" or send_anyway:
             send_mail(subject, message, uc.FROM_EMAIL_ADDRESS, [self.email])
+
+    def __str__(self):
+        # for security reasons, do not include the password field
+        exclude_fields = ["password"]
+        return super().__str__(exclude_fields)
+
+    def __repr__(self):
+        # for security reasons, do not include the password field
+        exclude_fields = ["password"]
+        return super().__repr__(exclude_fields)
