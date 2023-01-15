954c618428779e97d854ba1954d2adfc5dd09258
Rebecca Drabenstott <rdrabenstott@gmail.com>
2023-01-15 14:32:58 -0500

    Remove __str__ from PrintableModelMixin (#351)
    The Django Admin UI uses __str__ extensively, so it is better to revert to the less verbose __str__ implementations

--- dear_petition/common/models.py
+++ dear_petition/common/models.py
@@ -3,26 +3,12 @@ from itertools import chain
 
 class PrintableModelMixin(object):
     """
-    Mixin used by Models to implement str() and repr().  It includes all fields in the model including uneditable fields
+    Mixin used by Models to implement repr().  It includes all fields in the model including uneditable fields
     and many to many relationships. If desired, fields can be excluded from output.
 
     See discussion at:
     https://stackoverflow.com/questions/21925671/convert-django-model-object-to-dict-with-all-of-the-fields-intact
     """
-
-    def __str__(self, exclude_fields=[]):
-        """
-        Long values are truncated.
-
-        Example output:
-        Petition [id=1, created=2022-12-18 03:15:20.468997+00:00, modified=2022-12-18 03:15:20.468997+00:00,
-        form_type=AOC-CR-287, batch=1, county=Independent and Sovereign County of Durham North C..., jurisdiction=D,
-        offense_records=[1], agencies=[1, 2]]
-        """
-        max_length = 50
-        return '{} [{}]'.format(
-            self.__class__.__name__,
-            ', '.join('{}={}'.format(k, truncate(v, max_length)) for k, v in self.to_dict(exclude_fields).items()))
 
     def __repr__(self, exclude_fields=[]):
         """
@@ -44,12 +30,3 @@ class PrintableModelMixin(object):
             if f.name not in exclude_fields:
                 data[f.name] = [i.id for i in f.value_from_object(instance)]
         return data
-
-
-def truncate(value, max_length):
-    """
-    If value has more than max_length characters, truncate it to max_length and indicate it has been truncated by adding
-    "..." at the end of the value.  For example, truncate("Lorem ipsum dolor sit", 10) would return "Lorem ipsu..."
-    """
-    v = str(value)
-    return v if len(v) <= max_length else (v[:max_length] + '...')
--- dear_petition/petition/tests/test_models.py
+++ dear_petition/petition/tests/test_models.py
@@ -13,7 +13,7 @@ pytestmark = pytest.mark.django_db
 
 def test_printable_model_mixin_petition(batch, petition, contact1, contact2, offense_record1):
     """
-    Test PrintableModelMixin str() and repr() methods using Petition model because Petition has many to many
+    Test PrintableModelMixin repr() method using Petition model because Petition has many to many
     relationships.
     """
 
@@ -22,30 +22,14 @@ def test_printable_model_mixin_petition(batch, petition, contact1, contact2, off
     petition.agencies.add(contact2)
     petition.offense_records.add(offense_record1)
 
-    # set a value greater than 50 characters to test truncation
-    petition.county = "Independent and Sovereign County of Durham North Carolina"
-
     # assertions
-    expected_str = ", ".join([
-        "Petition [id=" + str(petition.id),
-        "created=" + str(petition.created),
-        "modified=" + str(petition.modified),
-        "form_type=AOC-CR-287",
-        "batch=" + str(batch.id),
-        "county=Independent and Sovereign County of Durham North C...",
-        "jurisdiction=D",
-        "offense_records=[" + str(offense_record1.id) + "]",
-        "agencies=[" + str(contact1.id) + ", " + str(contact2.id) + "]]"
-    ])
-    assert (str(petition) == expected_str)
-
     expected_repr = ", ".join([
         "{'id': " + repr(petition.id),
         "'created': " + repr(petition.created),
         "'modified': " + repr(petition.modified),
         "'form_type': 'AOC-CR-287'",
         "'batch': " + repr(batch.id),
-        "'county': 'Independent and Sovereign County of Durham North Carolina'",
+        "'county': 'DURHAM'",
         "'jurisdiction': 'D'",
         "'offense_records': [" + repr(offense_record1.id) + "]",
         "'agencies': [" + repr(contact1.id) + ", " + repr(contact2.id) + "]}"
@@ -55,31 +39,13 @@ def test_printable_model_mixin_petition(batch, petition, contact1, contact2, off
 
 def test_printable_model_mixin_user():
     """
-    Test PrintableModelMixin str() and repr() methods using User model because User has a field that should be excluded.
+    Test PrintableModelMixin repr() method using User model because User has a field that should be excluded.
     """
 
     # create model object
     user = UserFactory(username="tmarshall", email="tmarshall@supremecourt.gov", name="Thurgood Marshall")
 
     # assertions
-    expected_str = ", ".join([
-        "User [id=" + str(user.id),
-        "last_login=None",
-        "is_superuser=False",
-        "username=tmarshall",
-        "first_name=",
-        "last_name=",
-        "email=tmarshall@supremecourt.gov",
-        "is_staff=False",
-        "is_active=True",
-        "date_joined=" + str(user.date_joined),
-        "name=Thurgood Marshall",
-        "last_generated_petition_time=None",
-        "groups=[]",
-        "user_permissions=[]]"
-    ])
-    assert (str(user) == expected_str)
-
     expected_repr = ", ".join([
         "{'id': " + repr(user.id),
         "'last_login': None",
