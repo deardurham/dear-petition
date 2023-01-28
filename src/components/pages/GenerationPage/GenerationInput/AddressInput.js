d8dfdb520e8978e2929aee35ab88eebac1e51e8c
austin-bagwell <austinbagwell.dev@gmail.com>
2023-01-28 12:27:28 -0500

    only modifying stateObj in AddressInput

--- src/components/pages/GenerationPage/GenerationInput/AddressInput.js
+++ src/components/pages/GenerationPage/GenerationInput/AddressInput.js
@@ -26,7 +26,8 @@ const TextInput = styled(Input)`
 `;
 
 export default function AddressInput({ address, setAddress, disabled, errors, onClearError }) {
-  const { address1, address2, city, stateAsObject, zipcode } = address;
+  const { address1, address2, city, state, zipcode } = address;
+  const stateObj = typeof state === 'string' ? { value: state, label: state } : state;
   const handleChange = (key, value) => {
     if (disabled) return;
     setAddress((prev) => ({ ...prev, [key]: value }));
@@ -58,7 +59,7 @@ export default function AddressInput({ address, setAddress, disabled, errors, on
         <Select
           label="State"
           disabled={disabled}
-          value={stateAsObject}
+          value={stateObj}
           onChange={(value) => handleChange('state', value)}
           options={US_STATES.map((s) => ({ value: s[0], label: s[0] }))}
           errors={!disabled && errors.state}
