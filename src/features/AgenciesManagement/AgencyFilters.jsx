07ba013fa027f0aa45927891240765db71daa78a
Rob Gries <robert.w.gries@gmail.com>
2023-03-23 20:52:10 -0400

    Fix badges missing from contact filter

--- src/features/AgenciesManagement/AgencyFilters.jsx
+++ src/features/AgenciesManagement/AgencyFilters.jsx
@@ -4,6 +4,7 @@ import { useLazyGetContactFilterOptionsQuery } from '../../service/api';
 import { useModalContext } from '../../components/elements/Button/ModalButton';
 import { Button } from '../../components/elements/Button';
 import AutocompleteInput from '../../components/elements/Input/AutocompleteInput';
+import { Badge } from '../../components/elements/Badge/Badge';
 
 export const AgencyFiltersModal = ({ onFilter, filterSelections }) => {
   const { closeModal } = useModalContext();
@@ -32,22 +33,33 @@ const ContactFilter = ({ field, category, label, onFilter, filterSelections }) =
     );
   };
   return (
-    <AutocompleteInput
-      label={label}
-      selections={filterSelections}
-      showSelections
-      onSelect={(value) => addSelection(value)}
-      onRemoveSelection={(value) => removeSelection(value)}
-      fetchSuggestions={async (searchValue) => {
-        const data = await triggerSuggestionsFetch(
-          {
-            params: { field, category, search: searchValue },
-          },
-          true
-        ).unwrap();
-        return data.filter((suggestion) => !filterSelections.includes(suggestion));
-      }}
-    />
+    <>
+      <AutocompleteInput
+        label={label}
+        selections={filterSelections}
+        showSelections
+        onSelect={(value) => addSelection(value)}
+        onRemoveSelection={(value) => removeSelection(value)}
+        fetchSuggestions={async (searchValue) => {
+          const data = await triggerSuggestionsFetch(
+            {
+              params: { field, category, search: searchValue },
+            },
+            true
+          ).unwrap();
+          return data.filter((suggestion) => !filterSelections.includes(suggestion));
+        }}
+      />
+      <div className="flex flex-wrap max-h-[70px] mt-2 overflow-auto gap-2 select-none">
+        {filterSelections.map((selection, i) => (
+          <Badge
+            key={`${i}_${selection}`}
+            name={selection}
+            remove={() => removeSelection(selection)}
+          />
+        ))}
+      </div>
+    </>
   );
 };
 
