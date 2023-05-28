4fe18d6dcd1a4a06db97efd979e3960a9889960b
Rob Gries <robert.w.gries@gmail.com>
2023-05-28 14:00:27 -0400

    Remove prod build from local docker dev and fix eslint setup by separating prettier

--- src/components/elements/AutoSuggest/AutoSuggestionContainer.jsx
+++ src/components/elements/AutoSuggest/AutoSuggestionContainer.jsx
@@ -1,4 +1,3 @@
-import React from 'react';
 import styled from 'styled-components';
 import { colorWhite, colorBlack } from '../../../styles/colors';
 import keyAndAmbientShadows from '../../../styles/shadows';
