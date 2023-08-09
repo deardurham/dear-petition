f448bf2a294ffbaf8b9fe00bcbc6d37c60bc9648
Austin Bagwell <austinbagwell.dev@gmail.com>
2023-08-09 18:16:45 -0400

    errors - updated to use boilerplate vitest code

--- src/tests/App.test.jsx
+++ src/tests/App.test.jsx
@@ -1,7 +1,22 @@
-import { render, screen } from '@testing-library/react';
-import App from './components/App';
+// import { render, screen } from '@testing-library/react';
+// import App from './components/App';
 
-it('renders login page', () => {
-  render(<App />);
-  expect(screen.getByText('username')).toBeInTheDocument();
+// it('renders login page', () => {
+//   render(<App />);
+//   expect(screen.getByText('username')).toBeInTheDocument();
+// });
+
+import App from '../components/App';
+import { it, describe } from 'vitest';
+import { render, screen } from '@testing-library/react';
+
+describe('App.js', () => {
+  it('Check if the App renders even a tiny bit plz', () => {
+    //render our App properly
+    render(<App />);
+    const username = screen.getByText('username');
+    expect(username).toBeInTheDocument();
+
+    // screen.debug();
+  });
 });
