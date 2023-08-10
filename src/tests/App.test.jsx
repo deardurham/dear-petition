45ba199ea1f7d00da99386bd750b819c35356342
Austin Bagwell <austinbagwell.dev@gmail.com>
2023-08-09 21:24:35 -0400

    wrote test to test vitest functionality. is this recursive testing?

--- src/tests/App.test.jsx
+++ src/tests/App.test.jsx
@@ -6,17 +6,23 @@
 //   expect(screen.getByText('username')).toBeInTheDocument();
 // });
 
-import App from '../components/App';
-import { it, describe } from 'vitest';
-import { render, screen } from '@testing-library/react';
+// import App from '../components/App';
+// import HelpPage from '../components/pages/HelpPage/HelpPage';
+// import { it, describe, test } from 'vitest';
+import { test } from 'vitest';
+// import { render, screen } from '@testing-library/react';
 
-describe('App.js', () => {
-  it('Check if the App renders even a tiny bit plz', () => {
-    //render our App properly
-    render(<App />);
-    const username = screen.getByText('username');
-    expect(username).toBeInTheDocument();
-
-    // screen.debug();
-  });
+test('1 + 1 = 2', () => {
+  const sum = (x, y) => x + y;
+  expect(sum(1, 1)).toEqual(2);
 });
+
+// describe('App.js', () => {
+//   it('Check if the App renders even a tiny bit plz', () => {
+//     //render our App properly
+//     render(<HelpPage />);
+//     const contact = screen.getByText('Contact');
+//     expect(contact).toBeInTheDocument();
+//     screen.debug();
+//   });
+// });
