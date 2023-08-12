ec19ac03242be62d454ba53e2611eb89709037fc
Austin Bagwell <austinbagwell.dev@gmail.com>
2023-08-12 16:26:24 -0400

    attempt to render App inside a div container

--- src/__tests__/App.test.jsx
+++ src/__tests__/App.test.jsx
@@ -1,16 +1,19 @@
-// import { render, screen } from '@testing-library/react';
-// import App from '../components/App';
+import { render, screen } from '@testing-library/react';
+import App from '../components/App';
 import { test } from 'vitest';
 
-// test('When the app starts it renders a log in button', () => {
-//   render(<App />);
-//   const loginElement = screen.getByText('Log In');
-//   expect(loginElement).toBeInTheDocument();
-// });
+test('When the app starts it renders a log in button', () => {
+  const container = document.createElement('div');
+  container.setAttribute('id', 'test-root');
+  document.body.appendChild(container);
+  render(<App />, container);
+  const loginElement = screen.getByText('Log In');
+  expect(loginElement).toBeInTheDocument();
+});
 
 // Testing vitest to make sure it will run a very basic test correctly (spoiler, it does)
 // npm run test
-test('1 + 1 = 2', () => {
-  const sum = (x, y) => x + y;
-  expect(sum(1, 1)).toEqual(2);
-});
+// test('1 + 1 = 2', () => {
+//   const sum = (x, y) => x + y;
+//   expect(sum(1, 1)).toEqual(2);
+// });
