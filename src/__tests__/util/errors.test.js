eb0ed8f6b54ca0a3eafa4eb7ff49ab3e1cef3ad5
Austin Bagwell <austinbagwell.dev@gmail.com>
2023-08-13 15:39:46 -0400

    use vi.fn() to mock functions, passing all current tests

--- src/__tests__/util/errors.test.js
+++ src/__tests__/util/errors.test.js
@@ -1,50 +1,50 @@
 import { vi } from 'vitest';
 import { getErrorList, hasValidationsErrors } from '../../util/errors';
 
-const mockGetErrorList = {
-  mockErrors: {
-    errorOne: ['Error 1 message'],
-    errorTwo: ['Error 2 message'],
-  },
-  getErrorList,
-  hasValidationsErrors,
-};
-
 describe('Utils: errors.js', () => {
   afterEach(() => {
     vi.resetAllMocks();
   });
 
-  it('hasValidationErrors returns true if errors exist', () => {
-    const spy = vi.spyOn(mockGetErrorList, 'hasValidationsErrors');
-
-    expect(spy.getMockName()).toEqual('hasValidationsErrors');
-    expect(mockGetErrorList.hasValidationsErrors(mockGetErrorList.mockErrors)).toEqual(true);
-    expect(spy).toHaveBeenCalledTimes(1);
+  it('hasValidationsErrors returns true if errors exist', () => {
+    const hasValidationsErrorsSpy = vi.fn(hasValidationsErrors);
+    const mockErrors = {
+      errorOne: ['Error 1 message'],
+      errorTwo: ['Error 2 message'],
+    };
+    const hasErrors = hasValidationsErrorsSpy(mockErrors);
+    expect(hasErrors).toBe(true);
+    expect(hasValidationsErrorsSpy).toHaveBeenCalledTimes(1);
+    expect(hasValidationsErrorsSpy).toHaveReturned();
   });
 
-  it('hasValidationErrors returns false if no errors exist', () => {
-    const spy = vi.spyOn(mockGetErrorList, 'hasValidationsErrors');
-
-    expect(spy.getMockName()).toEqual('hasValidationsErrors');
-    expect(mockGetErrorList.hasValidationsErrors({})).toEqual(false);
-    expect(spy).toHaveBeenCalledTimes(1);
+  it('hasValidationsErrors returns false if no errors exist', () => {
+    const hasValidationsErrorsSpy = vi.fn(hasValidationsErrors);
+    const noErrors = hasValidationsErrorsSpy({});
+    expect(noErrors).toBe(false);
+    expect(hasValidationsErrorsSpy).toHaveBeenCalledTimes(1);
+    expect(hasValidationsErrorsSpy).toHaveReturned();
   });
 
   it('getErrorList returns a list of passed errors', () => {
-    const spy = vi.spyOn(mockGetErrorList, 'getErrorList');
-    expect(spy.getMockName()).toEqual('getErrorList');
+    const getErrorListSpy = vi.fn(getErrorList);
+    const mockErrors = {
+      errorOne: ['Error 1 message'],
+      errorTwo: ['Error 2 message'],
+    };
+    const expectedResult = [`ErrorOne: Error 1 message`, `ErrorTwo: Error 2 message`];
+    const result = getErrorListSpy(mockErrors);
 
-    const errorList = [`ErrorOne: Error 1 message`, `ErrorTwo: Error 2 message`];
-    expect(mockGetErrorList.getErrorList(mockGetErrorList.mockErrors)).toEqual(errorList);
-
-    expect(spy).toHaveBeenCalledTimes(1);
+    expect(result).toEqual(expectedResult);
+    expect(getErrorListSpy).toHaveBeenCalledTimes(1);
+    expect(getErrorListSpy).toHaveReturned();
   });
 
   it('getErrorList returns [] if no errors passed', () => {
     const getErrorListSpy = vi.fn(getErrorList);
     const noErrors = getErrorListSpy({});
     expect(noErrors).toStrictEqual([]);
+    expect(getErrorListSpy).toHaveBeenCalledTimes(1);
     expect(getErrorListSpy).toHaveReturned();
   });
 });
