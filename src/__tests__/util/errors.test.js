63ff2eee39eb7d7f107dc1bc2d2e7af0f1e224cf
Austin Bagwell <austinbagwell.dev@gmail.com>
2023-08-13 15:22:04 -0400

    fixed getErrorList 'no error passed' test

--- src/__tests__/util/errors.test.js
+++ src/__tests__/util/errors.test.js
@@ -23,6 +23,14 @@ describe('Utils: errors.js', () => {
     expect(spy).toHaveBeenCalledTimes(1);
   });
 
+  it('hasValidationErrors returns false if no errors exist', () => {
+    const spy = vi.spyOn(mockGetErrorList, 'hasValidationsErrors');
+
+    expect(spy.getMockName()).toEqual('hasValidationsErrors');
+    expect(mockGetErrorList.hasValidationsErrors({})).toEqual(false);
+    expect(spy).toHaveBeenCalledTimes(1);
+  });
+
   it('getErrorList returns a list of passed errors', () => {
     const spy = vi.spyOn(mockGetErrorList, 'getErrorList');
     expect(spy.getMockName()).toEqual('getErrorList');
@@ -33,12 +41,10 @@ describe('Utils: errors.js', () => {
     expect(spy).toHaveBeenCalledTimes(1);
   });
 
-  // FIXME
-  // declaring func and calling with {} returns [] in node REPL
-  // probably doing something wrong with vitest
   it('getErrorList returns [] if no errors passed', () => {
-    const spy = vi.spyOn(mockGetErrorList, 'getErrorList');
-    expect(spy.getMockName()).toEqual('getErrorList');
-    expect(mockGetErrorList.getErrorList({})).toEqual([]);
+    const getErrorListSpy = vi.fn(getErrorList);
+    const noErrors = getErrorListSpy({});
+    expect(noErrors).toStrictEqual([]);
+    expect(getErrorListSpy).toHaveReturned();
   });
 });
