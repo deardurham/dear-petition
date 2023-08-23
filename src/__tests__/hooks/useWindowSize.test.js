75d023a4d35a6df5b6722ae963bb315177975cec
Austin Bagwell <austinbagwell.dev@gmail.com>
2023-08-23 16:26:33 -0400

    reword assertion

--- src/__tests__/hooks/useWindowSize.test.js
+++ src/__tests__/hooks/useWindowSize.test.js
@@ -2,10 +2,12 @@ import useWindowSize from '../../hooks/useWindowSize';
 import { renderHook } from '@testing-library/react-hooks';
 
 describe('window resize hook', () => {
-  it('hook sets state to initial window size when called', () => {
+  it('sets state to initial window size', () => {
     const { result } = renderHook(() => useWindowSize());
 
     expect(result.current.height).not.toBeUndefined();
     expect(result.current.width).not.toBeUndefined();
+    expect(result.current.height).toBeGreaterThan(1);
+    expect(result.current.width).toBeGreaterThan(1);
   });
 });
