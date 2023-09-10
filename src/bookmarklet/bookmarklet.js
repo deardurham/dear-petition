01bbf10c4d15817155dfe415ead66dc7c4c9ee52
Rob Gries <robert.w.gries@gmail.com>
2023-09-10 16:19:00 -0400

    Automatically embed username

--- src/bookmarklet/bookmarklet.js
+++ src/bookmarklet/bookmarklet.js
@@ -1,37 +1,41 @@
-var username = prompt("Enter your Durham Expunction username");
-var html = document.documentElement.innerHTML;
-var loaded = 0;
+const POST_URL = import.meta.env.MODE === 'development' ? 'https://93d2-107-15-47-235.ngrok.io' : 'https://dear-petition.herokuapp.com/portal/bookmarklet/';
+import { minify } from "terser";
 
-var iframe = document.createElement('iframe');
-iframe.name = 'bookmarklet-' + Math.floor((Math.random() * 10000) + 1);
-iframe.style.display = 'none';
-
-iframe.onload = function () {
-    if (++loaded == 1) {
-        return;
-    }
-
-
-    document.body.removeChild(iframe);
-};
-
-var form = document.createElement('form');
-form.method = "POST";
-form.action = "https://27a9-99-92-50-211.ngrok-free.app/portal/bookmarklet/?nocache=" + Math.random();
-form.target = iframe.name;
-
-var username_input = document.createElement('input');
-username_input.name = 'user';
-username_input.value = username;
-
-var textarea = document.createElement('textarea');
-textarea.name = 'source';
-textarea.value = html;
-
-form.appendChild(username_input);
-form.appendChild(textarea);
-iframe.appendChild(form);
-
-document.body.appendChild(iframe);
-
-form.submit();
+export const generateBookmarklet = async (username) => (await minify(`
+    var html = document.documentElement.innerHTML;
+    var loaded = 0;
+    
+    var iframe = document.createElement('iframe');
+    iframe.name = 'bookmarklet-' + Math.floor((Math.random() * 10000) + 1);
+    iframe.style.display = 'none';
+    
+    iframe.onload = function () {
+        if (++loaded == 1) {
+            return;
+        }
+    
+    
+        document.body.removeChild(iframe);
+    };
+    
+    var form = document.createElement('form');
+    form.method = "POST";
+    form.action = '${POST_URL}/portal/bookmarklet/';
+    form.target = iframe.name;
+    
+    var username_input = document.createElement('input');
+    username_input.name = 'user';
+    username_input.value = '${username}';
+    
+    var textarea = document.createElement('textarea');
+    textarea.name = 'source';
+    textarea.value = html;
+    
+    form.appendChild(username_input);
+    form.appendChild(textarea);
+    iframe.appendChild(form);
+    
+    document.body.appendChild(iframe);
+    
+    form.submit();    
+`, { mangle: false })).code;
