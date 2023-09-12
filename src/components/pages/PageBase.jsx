b5a86f72d4b459db06bb232009a0f5673d59a712
Rob Gries <robert.w.gries@gmail.com>
2023-09-12 18:41:00 -0400

    Automatically embed username (#432)

--- src/components/pages/PageBase.jsx
+++ src/components/pages/PageBase.jsx
@@ -1,3 +1,4 @@
+import { useEffect, useState } from 'react';
 import { useDispatch } from 'react-redux';
 import { Link, useHistory } from 'react-router-dom';
 import styled from 'styled-components';
@@ -20,6 +21,7 @@ import { loggedOut } from '../../slices/auth';
 import { DropdownMenu } from '../elements/DropdownMenu';
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
 import { faCaretDown, faExternalLinkAlt } from '@fortawesome/free-solid-svg-icons';
+import { generateBookmarklet } from '../../bookmarklet/bookmarklet';
 
 const HeaderLogoLink = styled(LinkWrapper)`
   border: none;
@@ -56,8 +58,15 @@ function PageBase({ children, className, ...props }) {
   const { user } = useAuth();
   const dispatch = useDispatch();
   const [logout] = useLogoutMutation();
-  let bookmarkletJS = `var username=prompt("Enter your Durham Expunction username"),html=document.documentElement.innerHTML,loaded=0,iframe=document.createElement("iframe");iframe.name="bookmarklet-"+Math.floor(1e4*Math.random()+1),iframe.style.display="none",iframe.onload=function(){1!=++loaded&&document.body.removeChild(iframe)};var form=document.createElement("form");form.method="POST",form.action="https://27a9-99-92-50-211.ngrok-free.app/portal/bookmarklet/?nocache="+Math.random(),form.target=iframe.name;var username_input=document.createElement("input");username_input.name="user",username_input.value=username;var textarea=document.createElement("textarea");textarea.name="source",textarea.value=html,form.appendChild(username_input),form.appendChild(textarea),iframe.appendChild(form),document.body.appendChild(iframe),form.submit();`;
-  let bookmarklet = `javascript:(function(){${bookmarkletJS}})()`;
+  const [bookmarklet, setBookmarklet] = useState();
+
+  useEffect(() => {
+    const generate = async () => {
+      const bookmarkletCode = await generateBookmarklet(user.username);
+      setBookmarklet(`javascript:(function(){${bookmarkletCode}})()`)
+    }
+    generate();
+  }, [user.username]);
 
   return (
     <PageBaseStyled {...props}>
