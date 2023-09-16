0d8cf4ae2ebe9daf52591c14363e736e03491552
Austin Bagwell <austinbagwell.dev@gmail.com>
2023-09-16 15:07:51 -0400

    basic test outline - wip

--- src/__tests__/hooks/useBrowserWarning.test.js
+++ src/__tests__/hooks/useBrowserWarning.test.js
@@ -1,9 +1,23 @@
-import { renderHook } from '@testing-library/react-hooks';
+// import { setWindowProps } from '../util/isChrome.test';
+import { vi } from 'vitest';
+import { act, renderHook } from '@testing-library/react-hooks';
 import useBrowserWarning from '../../hooks/useBrowserWarning';
 
 describe('useBrowserWarning', () => {
-  it('if browser is Chrome, return [false, false]', () => {
-    const isChrome = true;
-    renderHook(() => useBrowserWarning());
+  const fn = vi.fn();
+
+  it('returns [false, () => setShouldDisplay(false)] when browser is Chrome', () => {
+    act(() => {
+      // error:
+      // Cannot set property userAgent of #<Navigator> which has only a getter
+      window.navigator.userAgent = `Mozilla/5.0 (X11; Linux x86_64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/51.0.2704.103 Safari/537.36`;
+      window.navigator.vendor = 'Google Inc.';
+      window.chrome = true;
+    });
+
+    const { result } = renderHook(() => useBrowserWarning());
+    console.log(`result.current`);
+    console.log(result.current);
+    expect(result.current).toBe([false, fn]);
   });
 });
