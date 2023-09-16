754c8cf6defeb3e0b67060dcb5fc63f251262e4c
Austin Bagwell <austinbagwell.dev@gmail.com>
2023-09-16 19:15:32 -0400

    attempt to set multiple window properties

--- src/__tests__/hooks/useBrowserWarning.test.js
+++ src/__tests__/hooks/useBrowserWarning.test.js
@@ -1,23 +1,74 @@
-// import { setWindowProps } from '../util/isChrome.test';
 import { vi } from 'vitest';
-import { act, renderHook } from '@testing-library/react-hooks';
+import { renderHook } from '@testing-library/react-hooks';
 import useBrowserWarning from '../../hooks/useBrowserWarning';
 
 describe('useBrowserWarning', () => {
-  const fn = vi.fn();
+  let windowSpy;
 
-  it('returns [false, () => setShouldDisplay(false)] when browser is Chrome', () => {
-    act(() => {
-      // error:
-      // Cannot set property userAgent of #<Navigator> which has only a getter
-      window.navigator.userAgent = `Mozilla/5.0 (X11; Linux x86_64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/51.0.2704.103 Safari/537.36`;
-      window.navigator.vendor = 'Google Inc.';
-      window.chrome = true;
-    });
-
-    const { result } = renderHook(() => useBrowserWarning());
-    console.log(`result.current`);
-    console.log(result.current);
-    expect(result.current).toBe([false, fn]);
+  beforeEach(() => {
+    windowSpy = vi.spyOn(window, 'window', 'get');
   });
+
+  afterEach(() => {
+    windowSpy.mockRestore();
+  });
+
+  it('renderHook does something useful', () => {
+    windowSpy.mockImplementation(() => ({
+      navigator: {
+        userAgent: 'bondJamesBond',
+        vendor: 'Google',
+      },
+      chrome: false,
+      opr: false,
+    }));
+    console.log(window);
+    const { result } = renderHook(() => useBrowserWarning());
+    console.log(result);
+
+    expect(window.navigator.userAgent).toEqual('bondJamesBond');
+  });
+
+  it('userAgent should be bondJamesBond', () => {
+    windowSpy.mockImplementation(() => ({
+      navigator: {
+        userAgent: 'bondJamesBond',
+      },
+    }));
+
+    expect(window.navigator.userAgent).toEqual('bondJamesBond');
+  });
+
+  it('should return https://example.com', () => {
+    windowSpy.mockImplementation(() => ({
+      location: {
+        origin: 'https://example.com',
+      },
+    }));
+
+    expect(window.location.origin).toEqual('https://example.com');
+  });
+
+  it('should be undefined.', () => {
+    windowSpy.mockImplementation(() => undefined);
+
+    expect(window).toBeUndefined();
+  });
+
+  //   const fn = vi.fn();
+
+  //   it('returns [false, () => setShouldDisplay(false)] when browser is Chrome', () => {
+  //     act(() => {
+  //       // error:
+  //       // Cannot set property userAgent of #<Navigator> which has only a getter
+  //       window.navigator.userAgent = `Mozilla/5.0 (X11; Linux x86_64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/51.0.2704.103 Safari/537.36`;
+  //       window.navigator.vendor = 'Google Inc.';
+  //       window.chrome = true;
+  //     });
+
+  //     const { result } = renderHook(() => useBrowserWarning());
+  //     console.log(`result.current`);
+  //     console.log(result.current);
+  //     expect(result.current).toBe([false, fn]);
+  //   });
 });
