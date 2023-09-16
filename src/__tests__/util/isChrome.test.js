cd8852e19f35f645c9dde12a51fca6a935aded23
Austin Bagwell <austinbagwell.dev@gmail.com>
2023-09-15 21:29:02 -0400

    add test that more closely mocks implementation

--- src/__tests__/util/isChrome.test.js
+++ src/__tests__/util/isChrome.test.js
@@ -2,10 +2,39 @@ import * as jdsom from 'jsdom';
 
 describe('isChrome test', () => {
   const { JSDOM, ResourceLoader } = jdsom;
+
+  it('testing imported isChrome', () => {
+    const loader = new ResourceLoader({
+      userAgent: 'Chrome/116.0.5845.187 (Official Build) <platform> (<platform-details>)',
+    });
+    const dom = new JSDOM(``, { resources: loader });
+    dom.window.chrome = true;
+
+    const isChromium = dom.window.chrome;
+    const winNav = dom.window.navigator;
+    // unable to set vendor on window object in JSDOM (yet?)
+    // const vendorName = winNav.vendor;
+    const vendorName = 'Google Inc.';
+    const isOpera = typeof window.opr !== 'undefined';
+    const isIEedge = winNav.userAgent.indexOf('Edge') > -1;
+    const isIOSChrome = winNav.userAgent.match('CriOS');
+
+    const isChrome =
+      !isIOSChrome &&
+      isChromium !== null &&
+      typeof isChromium !== 'undefined' &&
+      vendorName === 'Google Inc.' &&
+      isOpera === false &&
+      isIEedge === false;
+
+    expect(isChrome).toBe(true);
+  });
+
   it('userAgent includes Chrome, returns true', () => {
     const loader = new ResourceLoader({
       userAgent: 'Chrome/116.0.5845.187 (Official Build) <platform> (<platform-details>)',
     });
+
     const dom = new JSDOM(``, { resources: loader });
     const userAgent = dom.window.navigator.userAgent;
 
