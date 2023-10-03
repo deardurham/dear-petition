07769d805d6793dd7cf51fa00b9a3f2754ea842e
Austin Bagwell <austinbagwell.dev@gmail.com>
2023-10-03 09:53:42 -0400

    add resize event test

--- src/__tests__/hooks/useWindowSize.test.js
+++ src/__tests__/hooks/useWindowSize.test.js
@@ -14,4 +14,23 @@ describe('useWindowSize', () => {
 
     vi.unstubAllGlobals();
   });
+
+  it('adjusts after window resize event', () => {
+    vi.stubGlobal('innerWidth', 1920);
+    vi.stubGlobal('innerHeight', 1080);
+
+    const { result } = renderHook(() => useWindowSize());
+
+    expect(result.current.width).toBe(1920);
+    expect(result.current.height).toBe(1080);
+
+    window.innerWidth = 1280;
+    window.innerHeight = 720;
+    window.dispatchEvent(new Event('resize'));
+
+    expect(result.current.width).toBe(1280);
+    expect(result.current.height).toBe(720);
+
+    vi.unstubAllGlobals();
+  });
 });
