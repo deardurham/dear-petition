c0fbd05919d48c3e01df6be2f24ef1256850b527
Austin Bagwell <austinbagwell.dev@gmail.com>
2023-10-03 08:19:59 -0400

    mock initial window size with vi.stubGlobal

--- src/__tests__/hooks/useWindowSize.test.js
+++ src/__tests__/hooks/useWindowSize.test.js
@@ -1,13 +1,17 @@
 import useWindowSize from '../../hooks/useWindowSize';
+import { vi } from 'vitest';
 import { renderHook } from '@testing-library/react-hooks';
 
-describe('window resize hook', () => {
-  it('sets state to initial window size', () => {
+describe('useWindowSize', () => {
+  it('sets state to an initial window size', () => {
+    vi.stubGlobal('innerWidth', 1920);
+    vi.stubGlobal('innerHeight', 1080);
+
     const { result } = renderHook(() => useWindowSize());
 
-    expect(result.current.height).not.toBeUndefined();
-    expect(result.current.width).not.toBeUndefined();
-    expect(result.current.height).toBeGreaterThan(1);
-    expect(result.current.width).toBeGreaterThan(1);
+    expect(result.current.width).toBe(1920);
+    expect(result.current.height).toBe(1080);
+
+    vi.unstubAllGlobals();
   });
 });
