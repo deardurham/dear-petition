65874d3c472fc400cf0a8a06614d766f3c97e7b0
Austin Bagwell <austinbagwell.dev@gmail.com>
2023-10-05 21:17:08 -0400

    wip - using stubGlobal

--- src/__tests__/util/isChrome.test.js
+++ src/__tests__/util/isChrome.test.js
@@ -1,63 +1,64 @@
-import * as jdsom from 'jsdom';
+import { vi } from 'vitest';
 
 describe('isChrome test', () => {
-  const setWindowProps = ({ userAgent, vendor, chrome }) => {
-    const { JSDOM, ResourceLoader } = jdsom;
-    const loader = new ResourceLoader({
-      userAgent: userAgent,
+  beforeEach(() => {
+    vi.clearAllMocks();
+    vi.resetAllMocks();
+  });
+
+  afterEach(() => {
+    vi.stubGlobal('chrome', null);
+    vi.stubGlobal('navigator', {
+      userAgent: '',
+      vendor: '',
     });
-    const dom = new JSDOM(``, { resources: loader });
-    dom.window.chrome = chrome;
+  });
 
-    const isChromium = dom.window.chrome;
-    const winNav = dom.window.navigator;
-    // window.navigator.vendor is deprecated
-    // https://developer.mozilla.org/en-US/docs/Web/API/Navigator/vendor
-    // unable to set vendor on window object in JSDOM, readonly
-    // const vendorName = winNav.vendor;
-    const vendorName = vendor;
-    const isOpera = typeof window.opr !== 'undefined';
-    const isIEedge = winNav.userAgent.indexOf('Edge') > -1;
-    const isIOSChrome = winNav.userAgent.match('CriOS');
-
-    const isChrome =
-      !isIOSChrome &&
-      isChromium !== null &&
-      typeof isChromium !== 'undefined' &&
-      vendorName === 'Google Inc.' &&
-      isOpera === false &&
-      isIEedge === false;
-
-    return isChrome;
-  };
-
+  /*
   it('Mock Chrome userAgent, vendor in JSDOM', () => {
-    const isChrome = setWindowProps({
       userAgent: `Mozilla/5.0 (X11; Linux x86_64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/51.0.2704.103 Safari/537.36`,
       vendor: 'Google Inc.',
       chrome: true,
-    });
-
-    expect(isChrome).toBe(true);
-  });
 
   it('Mock Mozilla userAgent, vendor in JSDOM', () => {
-    const isChrome = setWindowProps({
       userAgent: `Mozilla/5.0 (Windows NT 6.1; Win64; x64; rv:47.0) Gecko/20100101 Firefox/47.0`,
       vendor: '',
       chrome: false,
-    });
-
-    expect(isChrome).toBe(false);
-  });
-
+  
   it('Mock iPhoneSafari userAgent, vendor in JSDOM', () => {
-    const isChrome = setWindowProps({
       userAgent: `Mozilla/5.0 (iPhone; CPU iPhone OS 13_5_1 like Mac OS X) AppleWebKit/605.1.15 (KHTML, like Gecko) Version/13.1.1 Mobile/15E148 Safari/604.1`,
       vendor: 'Apple Computer, Inc.',
       chrome: false,
     });
+  */
 
-    expect(isChrome).toBe(false);
+  it('browser is Chrome', async () => {
+    vi.stubGlobal('chrome', true);
+    vi.stubGlobal('navigator', {
+      userAgent: `Mozilla/5.0 (X11; Linux x86_64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/51.0.2704.103 Safari/537.36`,
+      vendor: 'Google Inc.',
+    });
+
+    const result = (await import('../../util/isChrome.js')).default;
+    expect(result).toEqual(true);
+  });
+
+  it('browser is Mozilla', async () => {
+    console.log('before new stubs');
+    console.log('window.chrome: ', window.chrome);
+    console.log('window.vendor: ', window.navigator.vendor);
+
+    vi.stubGlobal('chrome', undefined);
+    vi.stubGlobal('navigator', {
+      userAgent: `Mozilla/5.0 (Windows NT 6.1; Win64; x64; rv:47.0) Gecko/20100101 Firefox/47.0`,
+      vendor: '',
+    });
+
+    console.log('after new stubs');
+    console.log('window.chrome: ', window.chrome);
+    console.log('window.vendor: ', window.navigator.vendor);
+
+    const result = (await import('../../util/isChrome.js')).default;
+    expect(result).toEqual(false);
   });
 });
