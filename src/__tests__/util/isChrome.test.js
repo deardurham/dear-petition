5e10d51d00d441aae72096cf966f098e0ee10b7c
Austin Bagwell <austinbagwell.dev@gmail.com>
2023-10-08 14:44:44 -0400

    WIP - clean comments, test only Chrome, Mozilla

--- src/__tests__/util/isChrome.test.js
+++ src/__tests__/util/isChrome.test.js
@@ -14,25 +14,7 @@ describe('isChrome test', () => {
     });
   });
 
-  /*
-  it('Mock Chrome userAgent, vendor in JSDOM', () => {
-      userAgent: `Mozilla/5.0 (X11; Linux x86_64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/51.0.2704.103 Safari/537.36`,
-      vendor: 'Google Inc.',
-      chrome: true,
-
-  it('Mock Mozilla userAgent, vendor in JSDOM', () => {
-      userAgent: `Mozilla/5.0 (Windows NT 6.1; Win64; x64; rv:47.0) Gecko/20100101 Firefox/47.0`,
-      vendor: '',
-      chrome: false,
-  
-  it('Mock iPhoneSafari userAgent, vendor in JSDOM', () => {
-      userAgent: `Mozilla/5.0 (iPhone; CPU iPhone OS 13_5_1 like Mac OS X) AppleWebKit/605.1.15 (KHTML, like Gecko) Version/13.1.1 Mobile/15E148 Safari/604.1`,
-      vendor: 'Apple Computer, Inc.',
-      chrome: false,
-    });
-  */
-
-  it('browser is Chrome', async () => {
+  it('browser is Chrome, isChrome=true', async () => {
     vi.stubGlobal('chrome', true);
     vi.stubGlobal('navigator', {
       userAgent: `Mozilla/5.0 (X11; Linux x86_64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/51.0.2704.103 Safari/537.36`,
@@ -43,20 +25,12 @@ describe('isChrome test', () => {
     expect(result).toEqual(true);
   });
 
-  it('browser is Mozilla', async () => {
-    console.log('before new stubs');
-    console.log('window.chrome: ', window.chrome);
-    console.log('window.vendor: ', window.navigator.vendor);
-
+  it('browser is Mozilla, isChrome=false', async () => {
     vi.stubGlobal('chrome', undefined);
     vi.stubGlobal('navigator', {
       userAgent: `Mozilla/5.0 (Windows NT 6.1; Win64; x64; rv:47.0) Gecko/20100101 Firefox/47.0`,
       vendor: '',
     });
-
-    console.log('after new stubs');
-    console.log('window.chrome: ', window.chrome);
-    console.log('window.vendor: ', window.navigator.vendor);
 
     const result = (await import('../../util/isChrome.js')).default;
     expect(result).toEqual(false);
