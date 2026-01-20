3382e730147c11fd6b37000c1f85966440652009
Daniel Ulshen <dulshen@gmail.com>
2026-01-20 23:05:01 +0000

    removing validation rules for is_sheriff since default is false so this is no longer needed

--- src/features/CreateContact.jsx
+++ src/features/CreateContact.jsx
@@ -142,9 +142,6 @@ export const CreateContact = ({
           inputProps={{
             control,
             name: 'is_sheriff',
-            rules: {
-              required: false,
-            },
           }}
         />
       </form>
